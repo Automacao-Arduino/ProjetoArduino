8f87f12fdd086b1f6e204fe164ac8301ea2a1cf3
Vineww <vcvicari@gmail.com>
2024-11-06 18:50:20 -0300

    Melhora botão de trocar temperatura

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/button';
 import AntButton from './components/antButton';
+
+// Definindo o tipo para as props de ButtonCard
+interface ButtonCardProps {
+  ligarAr: () => void;
+  tempAr: number;
+}
 
 // Componente Header aprimorado
 function Header() {
@@ -13,15 +19,17 @@ function Header() {
 }
 
 // Componente ButtonCard para estilizar os botões em formato de card
-function ButtonCard({ ligarAr }) {
+function ButtonCard({ ligarAr, tempAr }: ButtonCardProps) {
   return (
     <div className="bg-cyan-400 shadow-md rounded-lg p-10 w-full max-w-md flex-grow">
-      <h2 className="text-2xl font-medium text-center mb-3 text-black">Ar Condicionado</h2>
-      <div className="space-y-4">
+      <h2 className="text-2xl font-medium text-center mb-1 text-black">Ar Condicionado</h2>
+      <div className="m-2 mt-1 p-3 w-full flex flex-col items-center">
         <Button label="Ligar Ar Condicionado" onClick={ligarAr} variant="primary" size="large" />
-        <h2 className="text-2xl font-medium text-center mb-4 text-black">Temperatura: 23°C</h2>
-        <div className="slider-temperatura w-full flex justify-center items-center p-4 pr-10">
-          <input type="range" min="1" max="100" value="50" className="slider w-3/4 h-2 bg-gray-300 rounded-lg appearance-none" id="myRange"></input>
+        <h2 className="text-2xl font-medium text-center mb-4 text-black mt-3">Temperatura: </h2>
+        <div className="flex items-center space-x-4">
+          <button className="bg-blue-500 text-white p-1 w-16 font-extrabold text-3xl rounded-full">+</button>
+          <input className="size-8 text-center w-16" value={tempAr} readOnly />
+          <button className="bg-blue-500 text-white p-1 w-16 font-extrabold text-3xl rounded-full">-</button>
         </div>
       </div>
     </div>
@@ -52,16 +60,21 @@ function Footer() {
 
 // Função App
 export function App() {
+  const [tempAr, setTempAr] = useState(23); 
+
   // Função de clique para os botões
   const ligarAr = () => {
     alert('Você ligou o Ar condicionado!');
   };
+
   const ligarSom = () => {
     alert('Você ligou o aparelho de som!');
   };
+
   const aumentarSom = () => {
     alert('Você aumentou o volume!');
   };
+
   const diminuirSom = () => {
     alert('Você diminuiu o volume!');
   };
@@ -70,7 +83,7 @@ export function App() {
     <div className="bg-gray-100 min-h-screen flex flex-col justify-between">
       <Header />
       <main className="flex-grow flex items-center justify-center py-10 space-x-4 max-w-4xl mx-auto">
-        <ButtonCard ligarAr={ligarAr} />
+        <ButtonCard ligarAr={ligarAr} tempAr={tempAr} /> {/* Passando tempAr para ButtonCard */}
         <ButtonCard2 ligarSom={ligarSom} aumentarSom={aumentarSom} diminuirSom={diminuirSom} />
       </main>
       <Footer />
