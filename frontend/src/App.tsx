8764df0ea2ee414900363b955ee1e06788fcb092
Fernando Borin <fernandopborin@gmail.com>
2024-11-19 12:17:45 -0300

    Projeto Front (Commit Atrasado, caiu por terra)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-neutral-900 flex justify-center p-6">
       <div className="w-[450px] h-[800px] bg-neutral-800 rounded-3xl shadow p-3"></div>
+      <div className="w-[450px] h-[800px] bg-neutral-800 rounded-3xl shadow p-3"></div>
     </div>
   );
 }
